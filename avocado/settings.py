6bb39c78f90c4e26e8a23da45cb898f0cb402cf6
EisenSkrt <85437983+EisenSkrt@users.noreply.github.com>
2021-12-27 16:46:29 +0200

    Update settings.py

--- avocado/settings.py
+++ avocado/settings.py
@@ -79,14 +79,10 @@ WSGI_APPLICATION = 'avocado.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-‘default’: {
-‘ENGINE’: ‘django.db.backends.postgresql_psycopg2’,
-‘NAME’: ‘my_db’,
-‘USER’ : ‘hero’,
-‘PASSWORD’ : ‘my_db@123’,
-‘HOST’ : ‘localhost’,
-‘PORT’ : ‘5432’,
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 # Password validation
